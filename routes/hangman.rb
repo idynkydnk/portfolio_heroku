61adcade0bae29991598d2474e582ebc371dd356
Kyle Thomson <idynkydnk@gmail.com>
2017-05-19 10:58:50 -0400

    accepts 6 guesses correctly

--- routes/hangman.rb
+++ routes/hangman.rb
@@ -1,33 +1,21 @@
 get '/hangman' do
   @session = session
-  @session[:word] = "hello"
   @guess = session.delete(:guess)
+  if @session[:guesses]
+    if @session[:guesses].length < 6
+      @session[:guesses] << @guess unless @guess == nil
+    else
+      session.delete(:guesses)
+      @session[:guesses] = []
+    end
+  else
+    @session[:guesses] = []
+    session[:guesses] << @guess unless @guess == nil
+  end
   slim :hangman
 end
 
 post '/hangman' do
   session[:guess] = params["guess"] 
   redirect '/hangman'
-end
-
-def caesar_cipher text, offset
-  text_array = text.split("")
-  x = 0
-  text_array.each do |letter|
-    if letter =~ /[a-z]/
-      new_letter = (letter.ord + offset)
-      if new_letter.chr > 'z'
-        new_letter -= 26
-      end
-      text_array[x] = new_letter.chr
-    elsif letter =~ /[A-Z]/
-      new_letter = (letter.ord + offset)
-      if new_letter.chr > 'Z'
-        new_letter -= 26
-      end
-    text_array[x] = new_letter.chr
-    end
-  x += 1
-  end
-  return text_array.join
 end
