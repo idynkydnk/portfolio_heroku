872d7dde5b32ebafbfe9953c2ee6d0f66aaa9692
Kyle Thomson <idynkydnk@gmail.com>
2018-10-17 16:52:35 -0700

    percent down complete

--- routes/percent_down.rb
+++ routes/percent_down.rb
@@ -1,15 +1,15 @@
 get '/percent_down' do
-  percent_down, percent_gain = "", ""
+  percent_down, percent_gain = "50",100
   slim :percent_down, :locals => {'percent_down' => percent_down, 'percent_gain' => percent_gain}
 end
 
 post '/percent_down' do
-  percent_down = params[:percent_down] || " "
-  percent_gain = calc_percent_gain(percent_down)
+  percent_down = params[:percent_down] || 0.0
+  percent_gain = calc_percent_gain(percent_down.to_f)
   slim :percent_down, :locals => {'percent_down' => percent_down, 'percent_gain' => percent_gain}
 end
 
 def calc_percent_gain percent_down
-  x = percent_down * 3
-  return x
+  u = percent_down / ( 1.0 - (0.01 * percent_down))
+  return u.round(2)
 end
