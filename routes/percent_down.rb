79e5ff690f5866aee1c4fe1c4d6dba3e769ea89e
Kyle Thomson <idynkydnk@gmail.com>
2018-10-17 14:23:36 -0700

    percent down page is good except algorithm

--- routes/percent_down.rb
+++ routes/percent_down.rb
@@ -6,27 +6,10 @@ end
 post '/percent_down' do
   percent_down = params[:percent_down] || " "
   percent_gain = calc_percent_gain(percent_down)
-  slim :percent_down, :locals => {'new_message' => new_message, 'message' => message, 'offset' => offset}
+  slim :percent_down, :locals => {'percent_down' => percent_down, 'percent_gain' => percent_gain}
 end
 
-def caesar_cipher text, offset
-  text_array = text.split("")
-  x = 0
-  text_array.each do |letter|
-    if letter =~ /[a-z]/
-      new_letter = (letter.ord + offset)
-      if new_letter.chr > 'z'
-        new_letter -= 26
-      end
-      text_array[x] = new_letter.chr
-    elsif letter =~ /[A-Z]/
-      new_letter = (letter.ord + offset)
-      if new_letter.chr > 'Z'
-        new_letter -= 26
-      end
-    text_array[x] = new_letter.chr
-    end
-  x += 1
-  end
-  return text_array.join
+def calc_percent_gain percent_down
+  x = percent_down * 3
+  return x
 end
